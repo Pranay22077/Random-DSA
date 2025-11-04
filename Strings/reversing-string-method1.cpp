c9c1cda8bcfa4e2d4f32a8fa838c0b36318ec4ef
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-04 21:03:41 +0530

    reversing string: method 1

---
+++ Strings/reversing-string-method1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char str[] = "pranay";
+    int i,j;
+    for (i = 0; str[i] != '\0'; i++){
+    }
+    i = i-1;
+    char str_cpy[10];
+    for ( j = 0; i != -1; i--,j++){
+        str_cpy[j] = str[i];
+    }
+    str_cpy[j] = '\0';
+    printf("Reversed string: %s", str_cpy);
+
+    return 0;
+
+}
