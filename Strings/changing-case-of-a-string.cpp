a03bc4f21b938a3486ffb60d9a1f0301ac7eb7f2
Pranay Gadh <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-04 06:28:03 +0530

    changing case code and counting vowels file

---
+++ Strings/changing-case-of-a-string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a string to be converted to upper case (one word only): ";
+    getline(cin, str);
+    cout << str;
+
+    for (int i = 0; str[i]!= '\0'; i++){
+        str[i] = str[i] - 32;
+    };
+
+    cout << "The changed string is: " << str;
+
+    return 0;
+}
