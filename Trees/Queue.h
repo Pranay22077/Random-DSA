4c14929e106e4a52756a26cb4df90844b0e3d6a8
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-10 19:11:05 +0530

    creating BTree

---
+++ Trees/Queue.h
@@ -0,0 +1,36 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <iostream>
+class Node{
+    int data;
+    Node *lchild, *rchild;
+    Node(int data){
+        this -> data = data;
+        lchild = rchild = nullptr;
+    }
+};
+
+class Queue {
+public:
+    int size;
+    Node* *A;
+    int p,q;
+
+    Queue(int val) {
+        size = val;
+        A = new Node*[size];
+        p = q = 0;
+    }
+
+    void enqueue(Node* num);
+    Node* dequeue();
+    void display();
+    bool isFull();
+    bool isEmpty();
+    ~Queue() {
+        delete[] A;
+    }
+};
+
+
+#endif
