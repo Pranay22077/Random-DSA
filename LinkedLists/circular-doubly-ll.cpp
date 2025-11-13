f830b1fc8afa6d0088e2bdcf74ff1d0f0127a0c1
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-13 15:08:17 +0530

    circular doubly linked list

---
+++ LinkedLists/circular-doubly-ll.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node *next, *prev;
+    Node(int data){
+        this -> data = data;
+        next = prev = nullptr;
+    }
+};
+
+class LinkedList{
+    public:
+    Node *head = nullptr;
+    void Create();
+    void Display();
+    ~LinkedList(){
+        delete head;
+    }
+};
+
+void LinkedList :: Create(){
+    Node *p = nullptr;
+    cout << "Enter the number of elements in the circular doubly linked-list: ";
+    int num;
+    cin >> num;
+    for (int i = 0; i<num; i++){
+        cout << "Enter element "<< i+1 << ": ";
+        int temp_data;
+        cin >> temp_data;
+        Node* temp = new Node(temp_data);
+        if (head == nullptr){
+            head = temp;
+            head -> next = head -> prev = head;
+            p = head;
+        } else {
+            p -> next = temp;
+            temp -> prev = p;
+            p = p -> next;
+        }
+    }
+    p -> next = head;
+    head -> prev = p;
+}
+
+void LinkedList :: Display(){
+    Node *ptr = head;
+    do{
+        cout << ptr -> data << " --> ";
+        ptr = ptr -> next;
+    } while (ptr != head);
+    cout << "(Reached Head!)";
+}
+
+int main(){
+    LinkedList list;
+    list.Create();
+    list.Display();
+
+    return 0;
+}
