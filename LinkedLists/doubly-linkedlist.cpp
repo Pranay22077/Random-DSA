5006e0752934d29ab2c23c90a8c7f00ddda0ba6b
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-13 14:14:47 +0530

    create function for doubly linkedlist

--- LinkedLists/doubly-linkedlist.cpp
+++ LinkedLists/doubly-linkedlist.cpp
@@ -20,3 +20,23 @@ class LinkedList{
         delete head;
     }
 };
+
+void LinkedList :: Create(){
+    Node *p = nullptr;
+    cout << "Enter the number of elements in the circular linked-list: ";
+    int num;
+    cin >> num;
+    for (int i = 0; i<num; i++){
+        cout << "Enter element "<< i+1 << ": ";
+        int temp_data;
+        cin >> temp_data;
+        Node* temp = new Node(temp_data);
+        if (head == nullptr){
+            head = temp;
+            head -> next = head -> prev = nullptr;
+            p = head;
+        } else {
+            p -> next = temp;
+            p -> next -> prev = p;
+        }
+}
