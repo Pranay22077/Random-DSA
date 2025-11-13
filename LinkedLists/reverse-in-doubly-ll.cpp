54de87773f3bd579572bb5d4a97921ac1d0d9913
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-13 15:01:14 +0530

    reverse a doubly linked list

---
+++ LinkedLists/reverse-in-doubly-ll.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node *next, *prev;
+    Node(int data){
+        this -> data = data;
+        next = prev = nullptr;
+    }
+};
+
+class LinkedList{
+    public:
+    Node *head = nullptr;
+    void Create();
+    void Insert();
+    void Delete();
+    void Reverse();
+    void Display();
+    void Display_rev();
+    ~LinkedList(){
+        delete head;
+    }
+};
+
+void LinkedList :: Create(){
+    Node *p = nullptr;
+    cout << "Enter the number of elements in the doubly linked-list: ";
+    int num;
+    cin >> num;
+    for (int i = 0; i<num; i++){
+        cout << "Enter element "<< i+1 << ": ";
+        int temp_data;
+        cin >> temp_data;
+        Node* temp = new Node(temp_data);
+        if (head == nullptr){
+            head = temp;
+            head -> next = head -> prev = nullptr;
+            p = head;
+        } else {
+            p -> next = temp;
+            temp -> prev = p;
+            p = p -> next;
+        }
+    }
+}
+
+void LinkedList :: Insert(){
+    cout << "Enter the element to be inserted: ";
+    int element;
+    cin >> element;
+
+    cout << "Enter the position at which it's to be inserted (starting index: 0): ";
+    int pos;
+    cin >> pos;
+
+    Node *nn = new Node(element);
+    
+    Node *p = head;
+    for (int i = 0; i < pos - 1; i++){
+        p = p -> next;
+    }
+    nn -> next = p -> next;
+    nn -> next -> prev = nn;
+
+    p -> next = nn;
+    nn -> prev = p;
+    }
+
+void LinkedList :: Delete(){
+    cout << "Enter the position at which the element needs to be deleted (starting index: 0): ";
+    int pos;
+    cin >> pos;
+    Node*p=  head;
+    for (int i = 0; i < pos-1; i++){
+        p = p -> next;
+    }
+    p -> next = p -> next -> next;
+    p -> next -> next -> prev = p;
+}
+
+void LinkedList :: Reverse(){
+    Node *p;
+    p = head;
+    p = p -> next;
+    while (p!= nullptr){
+        head = p;
+        p = p -> next;
+    }
+
+}
+
+void LinkedList :: Display(){
+    Node *ptr = head;
+    while (ptr != nullptr){
+        cout << ptr -> data << " <--> ";
+        ptr = ptr -> next;
+    }
+    cout << "NULL";
+}
+
+void LinkedList :: Display_rev(){
+    Node *ptr = head;
+    while (ptr != nullptr){
+        cout << ptr -> data << " <--> ";
+        ptr = ptr -> prev;
+    }
+    cout << "NULL";
+}
+
+int main(){
+    LinkedList list;
+    list.Create();
+    list.Display();
+
+    list.Insert();
+    list.Display();
+
+    list.Delete();
+    list.Display();
+cout << endl;
+    list.Reverse();
+    list.Display_rev();
+
+    return 0;
+}
