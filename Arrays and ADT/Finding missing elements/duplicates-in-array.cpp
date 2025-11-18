396e14f1f54fa0fc40cc216c61d71f258002c8a5
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-18 13:23:12 +0530

    duplicates in an array

---
+++ Arrays and ADT/Finding missing elements/duplicates-in-array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A[] = {3,6,8,8,10,12,15,15,15, 20};
+    int record = A[0];
+    for (int i = 0; i < 10; i++){
+        if (A[i] != record){
+            int j = 0, count = 0;
+            while (A[i + j] == A[i+j+1]){
+                count++;
+                j++;
+            }
+            if (count > 0){
+                cout << "Element "<< A[i] << " is repeated " << count << " times"<< endl;
+            }
+            record = A[i];
+        }
+    }
+}
