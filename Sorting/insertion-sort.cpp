0a4f50383701881c92e9516e97934fec14aa0188
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-11-20 12:38:02 +0530

    insertion sort

---
+++ Sorting/insertion-sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int A[], int size){
+    cout << size << endl;
+    for (int i = 1; i < size; i++){
+        int temp = A[i];
+        int j = i-1;
+        while (temp < A[j] && j >= 0){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = temp;
+    }
+}
+
+int main(){
+    int Arr[] = {4,3,8,4,9,7,2,1};
+    int size = sizeof(Arr)/ sizeof(Arr[0]);
+    insertion_sort(Arr, size);
+    for (int x : Arr){
+        cout << x << " ";
+    }
+
+    return 0;
+
+}
