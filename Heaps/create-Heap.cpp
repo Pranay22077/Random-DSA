3fafaf7bf31c18185bbf56bd65920c67767c33d7
Pranay <pranaygadh_mc24b06_003@dtu.ac.in>
2025-12-01 21:02:01 +0530

    Fix loop initialization and size calculation

--- Heaps/create-Heap.cpp
+++ Heaps/create-Heap.cpp
@@ -13,10 +13,11 @@ void Insert(int A[], int n){
 
 int main(){
     int H[] = {0, 40, 25, 10, 15, 30, 5, 35};
-    for (int i = 2; i< 8; i++){
+    int size = sizeof(A)/ sizeof(A[0]);
+    for (int i = 2; i< size; i++){
         Insert(H, i);
     }
-    for (int i = 1; i< 8; i++){
+    for (int i = 1; i< size; i++){
         cout << H[i] << " ";
     }
 
